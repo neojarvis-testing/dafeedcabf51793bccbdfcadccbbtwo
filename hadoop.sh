07962bdcfce52f0c4bec6e91e466c145accfd85e
neojarvis <techadmin@iamneo.ai>
2025-06-10 13:49:30 +0000

    Auto-save-commit-10/06/2025--19:19:27

--- hadoop.sh
+++ hadoop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo "Starting Hadoop daemons..."
+hdfs --daemon start namenode
+hdfs --daemon start datanode
+hdfs --daemon start secondarynamenode
+yarn --daemon start resourcemanager
+yarn --daemon start nodemanager
+echo "Hadoop started!"
+
 echo "🔍 Checking Hadoop..."
 
 # CLI Check
@@ -8,3 +16,6 @@ hdfs dfs -ls / || echo "❌ HDFS not responding"
 # Web UI Check
 echo "[🌐] Hadoop NameNode Web UI: https://ide-dafeedcabf51793bccbdfcadccbbtwo.ide1.exam.ly/proxy/9870/"
 echo "[🌐] Hadoop DataNode Web UI: https://ide-dafeedcabf51793bccbdfcadccbbtwo.ide1.exam.ly/proxy/9864/"
+echo "[🌐] Hadoop DataNode Web UI: https://ide-dafeedcabf51793bccbdfcadccbbtwo.ide1.exam.ly/proxy/8088/"
+echo "[🌐] Hadoop DataNode Web UI: https://ide-dafeedcabf51793bccbdfcadccbbtwo.ide1.exam.ly/proxy/8042/"
+
